f2412a2172349843d96c6720fae77b4d1f23b83e
Konstantin Pfrang <konstantinpfrang@znb63.zeuthen.desy.de>
2018-02-28 10:26:11 +0100

    clean up

--- konsta_cta/image.py
+++ konsta_cta/image.py
@@ -1,5 +1,4 @@
 import numpy as np
-
 # Calibration
 #############
 from ctapipe.calib.camera.r1 import HESSIOR1Calibrator
@@ -10,7 +9,6 @@ r1cal = HESSIOR1Calibrator()
 dl0cal = CameraDL0Reducer()
 dl1cal = CameraDL1Calibrator()
 
-
 def calibrator(event):
     r1cal.calibrate(event)
     dl0cal.reduce(event)
@@ -19,84 +17,86 @@ def calibrator(event):
 # Image Cleaning
 ################
 from ctapipe.image.cleaning import tailcuts_clean
-
 # Parametrization
 #################
 from ctapipe.image import hillas_parameters
 
 
 class ImagePreparer():
-    # Class to convert Hessio raw data from r0 data level
-    # to calibrated and cleaned images. Image cleaning
-    # performed using tailcut cleaning.
-
+    ''' Class to convert Hessio raw data from r0 data level
+        to calibrated and cleaned images. Image cleaning
+        performed using tailcut cleaning.
+    
+    '''
     def __init__(self, particles=None):
         # Input
         #######
         # particles - array of konsta_cta.readdata.FileReader
         #
-        self.cleaned_images = {}
+        self.clean_images = {}
         self.hillas_moments = {}
         self.n_images = {}
-        self.n_images_after_cleaning = {}
+        self.n_clean_images = {}
         self.particles = particles
         if not self.particles:
-            raise ValueError('Array of particles must be given.')
+            raise ValueError('No particle array given.')
         self.keys = None
         self.geoms = self.get_all_camera_geoms()
 
-
     def analysis(self):
-        # Performs the processing of the images:
-        # calibration perfoming conversion from r0 to dl1
-        # image cleaning using tailcut_cleaning
-        # image parametrization with Hillas parameters
-        #
-        # Fills the dicts for cleaned_images and hillas moments
+        ''' Performs the processing of the images:
+            calibration perfoming conversion from r0 to dl1
+            image cleaning using tailcut_cleaning
+            image parametrization with Hillas parameters
+            
+            Fills the dicts for clean_images and hillas moments
+        '''
 
         # loop through all particle types
         for particle in self.particles:
+            # count number of images and number after cleaning
             self.n_images[particle.datatype] = 0
-            self.n_images_after_cleaning[particle.datatype] = 0
+            self.n_clean_images[particle.datatype] = 0
+            dtype = particle.datatype
 
             # loop through all events
             for event in particle.source:
+                event_id = event.dl0.event_id
+
                 # loop through all telescopes with data
-                for tel_with_data in event.r0.tels_with_data:
-                    self.n_images[particle.datatype] += 1
-                    # calibrate data
+                for tel_id in event.r0.tels_with_data:
+                    self.n_images[dtype] += 1
                     calibrator(event)
                     # calibrated image
-                    image = event.dl1.tel[tel_with_data].image
-                    # select only the first gain and reshape array
+                    image = event.dl1.tel[tel_id].image
                     image = np.reshape(image[0], np.shape(image)[1])
-                    # camera geometry
-                    # determine image and boundary pixels
+                    # image cleaning
                     mask = tailcuts_clean(
-                        self.geoms[tel_with_data], image, min_number_picture_neighbors=2)
+                        self.geoms[tel_id], image,
+                        min_number_picture_neighbors=2
+                        )
 
                     # drop images that didn't survive image cleaning
                     if any(mask == True):
-                        self.n_images_after_cleaning[particle.datatype] += 1
-                        
-                        self.cleaned_images[particle.datatype,
-                                            event.dl0.event_id, tel_with_data] = np.copy(image)
+                        self.n_clean_images[dtype] += 1
+                        self.clean_images[dtype, event_id, tel_id] = np.copy(image)
                         # set rejected pixels to zero
-                        self.cleaned_images[particle.datatype,
-                                            event.dl0.event_id, tel_with_data][~mask] = 0
-                        
-                        self.hillas_moments[particle.datatype, event.dl0.event_id, tel_with_data] = hillas_parameters(
-                            self.geoms[tel_with_data], self.cleaned_images[particle.datatype, event.dl0.event_id, tel_with_data])
+                        self.clean_images[dtype, event_id, tel_id][~mask] = 0
 
-            print("Processed {} images for datatype {}. Images that didn't survive cleaning: {}".format(
-                self.n_images[particle.datatype], particle.datatype, self.n_images[
-                    particle.datatype] - self.n_images_after_cleaning[particle.datatype]))
+                        self.hillas_moments[dtype, event_id, tel_id] = hillas_parameters(
+                            self.geoms[tel_id], self.clean_images[dtype, event_id, tel_id]
+                                )
+            print("Processed {} images for datatype {}. Images" 
+                   "that didn't survive cleaning: {}".format(
+                    self.n_images[dtype], dtype,
+                    self.n_images[dtype] - self.n_clean_images[dtype])
+                  )
         self.get_keys()
 
     def get_keys(self):
         # returns an numpy array of the keys
         # (used later for accessing the cleaned images)
-        self.keys = np.array(list(self.cleaned_images.keys()))
+        self.keys = np.array(list(self.clean_images.keys()))
 
     def get_all_camera_geoms(self):
         # returns a dict of all camera geometries
@@ -105,17 +105,21 @@ class ImagePreparer():
             # get the first event:
             for event in particle.source:
                 break
+
             # read the camera imformations for al telescopes
             _geoms = {}
             for tel_id in event.inst.subarray.tel_id.item():
                 _geoms[tel_id] = event.inst.subarray.tel[tel_id].camera
 
-            # Test of the simulation files for all particle files use the same geometry
+            # Test of the simulation files for all particle files use the same
+            # geometry
             if not geoms:
                 geoms = _geoms
             else:
                 if geoms == _geoms:
                     pass
                 else:
-                    raise Exception('Not same camera geometries for the files occured for: {}'.format(particle.datatype))
+                    raise Exception('Not same camera geometries for the files'
+                        'occured for: {}'.format(particle.datatype)
+                        )
         return(geoms)
