9ed8b8d3af94e4b3d26cad2f420626d173c7c6e4
Konstantin Pfrang <konstantin.pfrang@desy.de>
2018-08-23 10:58:12 +0200

    Cleaning quality cut methods

--- konsta_cta/cutter.py
+++ konsta_cta/cutter.py
@@ -14,8 +14,8 @@ class Cutter(object):
     Base class to reject objects and apply quality cuts.
     '''
 
-    def __init__(self, quality_cuts):
-        self.quality_cuts = quality_cuts
+    def __init__(self):
+        pass
 
     def get_edge_pixels(self, camera, rows=1):
         '''
@@ -51,75 +51,148 @@ class Cutter(object):
 
         return neigh
 
-    def leakage_radius(self, camera, hillas_parameters,
-                       radius=0.8, method="mean"):
+    def leakage_cut(self, camera, hillas_parameters=None,
+                    radius=0.8, max_dist="mean", image=None,
+                    rows=2, fraction=0.15, method="radius"):
         '''
-        Handeling of images at the edge of the camera. Pictures
-        with c.o.g. (hillas r) outside of radius will be rejected.
+        Handeling of images at the edge of the camera.
 
         Parameters
         ----------
-        camera : event.inst.subarray.tel camera information
-        hillas_parameters : HillasParametersContainer
-        radius : maximum relative distance of cog to center
-            of the camera
-        method : ["mean", "max"]
-            Method to calculate the total size of the camera
+        method : string
+        camera : CameraDesctription
+            ctapipe camera disctription
+        hillas_parameters : HillasParametersContainer or None
+            Container with hillas parameters stored. Required for
+            method "radius
+        radius : float
+            maximum relative distance of cog to center of the camera
+        max_size : ["mean", "max"]
+            Method to calculate the total size of the camera. If "mean",
+            the average distance from the boundary pixels to the center
+            are considered. If "max", the distance to the pixel furthest
+            away is taken.
+        image : ndarray
+            image in camera in pe
+        rows : integer
+            number of rows defining edge for fraction method
+        fraction : float
+            maximum fraction of charge contained in edge of camera
 
         Returns
         -------
-        True if the cog is within accepted radius, otherwiese Falses
+        True if the cog is within accepted radius, False otherwise
         '''
 
-        # get the maximum distance from camera
-        if method == "max":
-            max_dist_camera = max(np.sqrt(camera.pix_x ** 2 + camera.pix_y ** 2))
+        if method == "None":
+            pass
 
-        elif method == "mean":
+        elif method == "radius":
+            # get the maximum distance from camera
+            if max_dist == "max":
+                max_dist_camera = max(np.sqrt(camera.pix_x ** 2 + camera.pix_y ** 2))
+
+            elif max_dist == "mean":
+                try:
+                    edge_pix = self.edge_pixels[camera.cam_id]
+                except KeyError:
+                    edge_pix = self.get_edge_pixels(camera)
+                    self.edge_pixels[camera.cam_id] = edge_pix
+
+                max_dist_camera = sum(np.sqrt(camera.pix_x ** 2 + \
+                                              camera.pix_y ** 2)[edge_pix]) / len(edge_pix)
+            else:
+                raise KeyError("The method {} for calculating "
+                               "max_dist_camera is not known.".format(method))
+
+            accepted_dist = radius * max_dist_camera
+
+            return (hillas_parameters.r < accepted_dist)
+
+        elif method == "fraction":
             try:
                 edge_pix = self.edge_pixels[camera.cam_id]
             except KeyError:
-                edge_pix = self.get_edge_pixels(camera)
+                edge_pix = self.get_edge_pixels(camera, rows)
                 self.edge_pixels[camera.cam_id] = edge_pix
 
-            max_dist_camera = sum(np.sqrt(camera.pix_x ** 2 + \
-                                          camera.pix_y ** 2)[edge_pix]) / len(edge_pix)
+            # fraction of charge in edge
+            frac = sum(image[edge_pix]) / sum(image)
+
+            return frac <= fraction
+
         else:
-            raise KeyError("The method {} for calculating "
-                           "max_dist_camera is not known.".format(method))
+            raise ValueError("Leakage method {} not known.".format(method))
 
-        accepted_dist = radius * max_dist_camera
-
-        return (hillas_parameters.r < accepted_dist)
-
-    def leakage_fraction(self, camera, image, rows=2, fraction=0.15):
-        '''
-        Reject images if the fraction of charge in outer pixels
-        is above the given value.
-
-        Parameters
-        ----------
-        camera : event.inst.subarray.tel camera information
-        image : image in camera in pe
-        rows : number of rows defining edge
-        fraction : maximum fraction of charge contained
-            in edge of camera
-
-        Returns
-        -------
-        True if leakage cut was passed, False otherwise
-        '''
-
-        try:
-            edge_pix = self.edge_pixels[camera.cam_id]
-        except KeyError:
-            edge_pix = self.get_edge_pixels(camera, rows)
-            self.edge_pixels[camera.cam_id] = edge_pix
-
-        # fraction of charge in edge
-        frac = sum(image[edge_pix]) / sum(image)
-
-        return frac <= fraction
+    # def leakage_radius(self, camera, hillas_parameters,
+    #                    radius=0.8, method="mean"):
+    #     '''
+    #     Handeling of images at the edge of the camera. Pictures
+    #     with c.o.g. (hillas r) outside of radius will be rejected.
+    #
+    #     Parameters
+    #     ----------
+    #     camera : event.inst.subarray.tel camera information
+    #     hillas_parameters : HillasParametersContainer
+    #     radius : maximum relative distance of cog to center
+    #         of the camera
+    #     method : ["mean", "max"]
+    #         Method to calculate the total size of the camera
+    #
+    #     Returns
+    #     -------
+    #     True if the cog is within accepted radius, otherwiese Falses
+    #     '''
+    #
+    #     # get the maximum distance from camera
+    #     if method == "max":
+    #         max_dist_camera = max(np.sqrt(camera.pix_x ** 2 + camera.pix_y ** 2))
+    #
+    #     elif method == "mean":
+    #         try:
+    #             edge_pix = self.edge_pixels[camera.cam_id]
+    #         except KeyError:
+    #             edge_pix = self.get_edge_pixels(camera)
+    #             self.edge_pixels[camera.cam_id] = edge_pix
+    #
+    #         max_dist_camera = sum(np.sqrt(camera.pix_x ** 2 + \
+    #                                       camera.pix_y ** 2)[edge_pix]) / len(edge_pix)
+    #     else:
+    #         raise KeyError("The method {} for calculating "
+    #                        "max_dist_camera is not known.".format(method))
+    #
+    #     accepted_dist = radius * max_dist_camera
+    #
+    #     return (hillas_parameters.r < accepted_dist)
+    #
+    # def leakage_fraction(self, camera, image, rows=2, fraction=0.15):
+    #     '''
+    #     Reject images if the fraction of charge in outer pixels
+    #     is above the given value.
+    #
+    #     Parameters
+    #     ----------
+    #     camera : event.inst.subarray.tel camera information
+    #     image : image in camera in pe
+    #     rows : number of rows defining edge
+    #     fraction : maximum fraction of charge contained
+    #         in edge of camera
+    #
+    #     Returns
+    #     -------
+    #     True if leakage cut was passed, False otherwise
+    #     '''
+    #
+    #     try:
+    #         edge_pix = self.edge_pixels[camera.cam_id]
+    #     except KeyError:
+    #         edge_pix = self.get_edge_pixels(camera, rows)
+    #         self.edge_pixels[camera.cam_id] = edge_pix
+    #
+    #     # fraction of charge in edge
+    #     frac = sum(image[edge_pix]) / sum(image)
+    #
+    #     return frac <= fraction
 
     def size_cut(self, hillas_parameters, min_size):
         '''
@@ -154,21 +227,20 @@ class Cutter(object):
 
         n_tels_per_type = {tel: len(tels_per_type[tel]) for tel in tels_per_type}
 
-        if (method == "None"):
+        if method == "None":
             '''
             Skip the multiplicity cut
             '''
             reject_event = False
 
-        elif (method == "total"):
+        elif method == "total":
             '''
             apply a cut on the total number of images per telescope.
             '''
             nimages = sum(n_tels_per_type.values())
             reject_event = (nimages < min_multiplicity["total"])
 
-
-        elif (method == "per_type"):
+        elif method == "per_type":
             '''
             check total telescope numbers per telescope. keep event unless
             all telescope types do not pass the cut or the total amound of
@@ -182,30 +254,29 @@ class Cutter(object):
 
             reject_event = (reject_total or reject_per_type)
 
-
-        elif (method == "per_type_remove"):
-            '''
-            remove telescope types that do not pass n_tels_per_type cut and
-            check if total number of images that are left pass n_tels cut. 
-            '''
-            limits = [min_multiplicity[tel] for tel in n_tels_per_type]
-            reject_per_type = np.less(list(n_tels_per_type.values()), limits)
-
-            rejected_types = np.array(list(n_tels_per_type.keys()))[reject_per_type]
-
-            # for tel in rejected_types:
-            #	del n_tels_per_type[tel]
-            #	del tels_per_type[tel]
-
-            nimages = sum(np.array(list(n_tels_per_type.values()))[~reject_per_type])
-            reject_event = (nimages < min_multiplicity["total"])
+        # elif method == "per_type_remove":
+        #     '''
+        #     remove telescope types that do not pass n_tels_per_type cut and
+        #     check if total number of images that are left pass n_tels cut.
+        #     '''
+        #     limits = [min_multiplicity[tel] for tel in n_tels_per_type]
+        #     reject_per_type = np.less(list(n_tels_per_type.values()), limits)
+        #
+        #     rejected_types = np.array(list(n_tels_per_type.keys()))[reject_per_type]
+        #
+        #     # for tel in rejected_types:
+        #     #	del n_tels_per_type[tel]
+        #     #	del tels_per_type[tel]
+        #
+        #     nimages = sum(np.array(list(n_tels_per_type.values()))[~reject_per_type])
+        #     reject_event = (nimages < min_multiplicity["total"])
 
         else:
             raise KeyError("Method {} not known for multiplicity_cut.".format(method))
 
         if reject_event:
             raise MultiplicityException("Not enough telescopes in in event.")
-        elif (method == "per_type_remove"):
-            return rejected_types
-        else:
-            return np.array([])  # no type removed
+        # elif (method == "per_type_remove"):
+        #     return rejected_types
+        # else:
+        #     return np.array([])  # no type removed
